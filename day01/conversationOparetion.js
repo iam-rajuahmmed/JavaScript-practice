0ce0a291dac5dd0623afb12ce483d759c3b59d7b
Raju Ahmmed <69457682+iam-rajuahmmed@users.noreply.github.com>
2025-12-18 08:58:52 +0000

    Conversation

---
+++ day01/conversationOparetion.js
@@ -0,0 +1,28 @@
+console.log("--this is String Convart");
+let score = 50;
+
+let number =  String(score);
+
+console.log(number);
+console.log(typeof(number));
+
+console.log("--this is NUmber Convart");
+
+
+
+let score2 = "50";
+let number2 = Number(score2);
+console.log(number2);
+console.log(typeof(number2));
+
+console.log("--this is Not a Number(NaN)");
+
+
+let score3 = "Karim Uddin";
+let number3 = Number(score2);
+console.log(number3);
+console.log(typeof(number3));
+console.log(number3);
+
+
+
