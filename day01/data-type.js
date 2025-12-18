cb05dfd8f0a81d52938973c36d57ecd1aebb0e9e
Raju Ahmmed <69457682+iam-rajuahmmed@users.noreply.github.com>
2025-12-18 08:36:19 +0000

    data Type

---
+++ day01/data-type.js
@@ -0,0 +1,16 @@
+const name = "Raju Ahmmed"; //String Type
+const id = 2535; // number type
+const thisisID = true; // Boolean type
+let fastName; //Undifined
+let lastName = null; //Standalone Value
+// Symbol type
+// Object type
+
+console.log(typeof id);
+
+
+
+
+console.log(name);
+console.log(id);
+console.log(name, id);
